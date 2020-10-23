8b72f2a6944bb9351954ff20eb3cc88a1641ec81
Huy <minhhuy8137@gmail.com>
2020-10-23 02:29:42 +0200

    update

--- src/pages/Favorite.vue
+++ src/pages/Favorite.vue
@@ -5,7 +5,10 @@
         <PokemonCardSkeleton v-for="index in 20" :key="index" />
       </div>
       <div v-if="loaded && noFavorite" class="absolute-center">
-        <h3>There is no favorite pokemon yet, try to capture some ;)</h3>
+        <h3>
+          There is no favorite pokemon yet <br />
+          Try to capture some ;)
+        </h3>
       </div>
       <div
         v-if="loaded && !noFavorite"
